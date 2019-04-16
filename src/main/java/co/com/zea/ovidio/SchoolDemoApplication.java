cbf1f8068b871776f82271380a895425d8aa7f72
OvidioZea <ovidio.zea@gmail.com>
2019-04-16 16:48:43 -0500

    Se agrega la configuración a Eureka

--- src/main/java/co/com/zea/ovidio/SchoolDemoApplication.java
+++ src/main/java/co/com/zea/ovidio/SchoolDemoApplication.java
@@ -2,8 +2,10 @@ package co.com.zea.ovidio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SchoolDemoApplication {
 
 	public static void main(String[] args) {
